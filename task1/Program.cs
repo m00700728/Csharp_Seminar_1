7d03a1802f892d34dc99e4789d5ba2705757df53
Morozov <morozov7777777@mail.ru>
2023-12-23 15:17:36 +0300

    Task1_done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        // Введите свое решение ниже
+        if (firstNumber > secondNumber) {
+            Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
+        } else if (firstNumber < secondNumber) {
+            Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
+        } else if (firstNumber == secondNumber) {
+             Console.WriteLine($"Введенные числа равны `{firstNumber}`");
+        }
+
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int firstNumber, secondNumber;
+
+        if (args.Length >= 2) {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 6;
+            secondNumber = 5;
+        }
+
+        // Не удаляйте строки ниже
+        CompareNumbers(firstNumber, secondNumber);
+    }
+}
